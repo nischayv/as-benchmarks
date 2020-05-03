320b3916183dc5dba1119615d56467a214ce6517
nischayv <nischay.v@gmail.com>
2020-05-02 20:09:33 -0700

    add ability to run benchmarks individually

--- computation/assembly/spmv.ts
+++ computation/assembly/spmv.ts
@@ -146,7 +146,7 @@ function genRand(lb: i32, hb: i32): i32 {
 // @ts-ignore: decorator
 @inline
 function randf(): f64 {
-  return 1.0 - 2.0 * (rand() as f64 / (2147483647 + 1.0))
+  return 1.0 - 2.0 * ((rand() as f64) / (2147483647 + 1.0))
 }
 
 function sortArray(a: StaticArray<u32>, start: u32, finish: u32): void {
